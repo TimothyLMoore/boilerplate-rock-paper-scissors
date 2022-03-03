9c6f3b792119ca2d2e23915ec1ce0026ce52fe68
Tim Moore <timoore92@gmail.com>
2022-03-03 17:09:07 -0400

    feat: program works
    Needs work:
    Model works but doesn't pass 2/4 test cases
    Player function needs improvement it runs too slow

--- RPS.py
+++ RPS.py
@@ -1,11 +1,37 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 import random
+from keras.models import Sequential, save_model, load_model
+import pandas as pd
+import numpy as np
+import pickle
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+def player(prev_play, opponent_history=["R","P","S"]):
+    with open("./model/le.obj", 'rb') as f:
+        le = pickle.load(f)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    filepath = "./model"
+    model = load_model(filepath, compile = True)
+    if not(prev_play == ""):
+        opponent_history.append(prev_play)
+    df = pd.DataFrame(opponent_history, columns = ['throws'])
+    df = pd.get_dummies(df['throws'])
+    if len(df) < 10:
+        df_head = pd.DataFrame(np.zeros((10 - len(df), 3)), columns = ["P", "R", "S"])
+        df = pd.concat([df_head,df])
+    df = df[-10:]
+    np_df = df.to_numpy()
 
-    return random.choice(['R', 'P', 'S'])
+    X = np_df.flatten()
+    #print(np.shape(X))
+    X = np.reshape(X,(1,30,1))
+    #print(X)
+    #print(np.shape(X))
+
+    pred = model.predict(X)
+
+
+    max_index = [np.argmax(pred)]
+
+    pred = str(le.inverse_transform(max_index))[2]
+
+    return pred #random.choice(['R',"P","S"])
--- train.py
+++ train.py
@@ -7,12 +7,13 @@ import pandas as pd
 
 import pickle
 
-from keras.models import Sequential
+from keras.models import Sequential, save_model, load_model
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 import tensorflow as tf
@@ -23,20 +24,29 @@ import tensorflow as tf
 def build_model():
 
     model = Sequential()
-    model.add(Dense(20, input_dim = 30, activation = 'relu')) # Rectified Linear Unit Activation Function
+    model.add(Dense(60, input_dim = 30, activation = 'relu')) # Rectified Linear Unit Activation Function
+    model.add(Dense(40, activation = 'relu'))
+    model.add(Dense(20, activation = 'relu'))
     model.add(Dense(10, activation = 'relu'))
-    model.add(Dense(5, activation = 'sigmoid'))
     model.add(Dense(3, activation = 'softmax')) # Softmax for multi-class classification
     # Compile model here
     model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
     return model
 
 if __name__ == '__main__':
-    model = build_model()
+    filepath = "./model"
+    model = load_model(filepath, compile = True)
     played = []
-    for i in range(1,100):
-        _, j = play(player, random_player, 1000)
+    for i in range(1,15):
+        #_, j = play(player, quincy, 100)
+        #played.append(j)
+        _, j = play(player, abbey, 100)
         played.append(j)
+        _, j = play(player, kris, 100)
+        played.append(j)
+        #_, j = play(player, mrugesh, 100)
+        #played.append(j)
+        print(i)
     played = list(itertools.chain(*played))
     winVs = {"S":"R", "R":"P", "P":"S"}
     y = [winVs[i] for i in played]
@@ -49,19 +59,27 @@ if __name__ == '__main__':
     np_df = df.to_numpy()
     for i in range(0,len(df)-10):
         X = np.concatenate([X,np_df[i:i+10].flatten()])
-        print(i)
+        if i % 1000 == 0:
+            print(i)
     X = X.reshape(len(X)//30,30)
 
 
-    encoder = LabelEncoder()
-    encoder.fit(y)
-    y = encoder.transform(y)
+    le = LabelEncoder()
+    le.fit(y)
+    y = le.transform(y)
+
     y = tf.keras.utils.to_categorical(y, num_classes=3, dtype="int")
+
+
+    with open("./model/le.obj", 'wb') as f:
+        pickle.dump(le, f)
+
     print(np.shape(X),np.shape(y))
 
-    model.fit(X,y,epochs=20, batch_size=100)
+    model.fit(X,y, epochs = 100, batch_size=100)
 
-    model.save("./model/model.h5")
+    filepath = "./model"
+    save_model(model, filepath)
 
 
 
